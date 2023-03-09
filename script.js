2c4b6d2b72b0c8b61038f02609d33dab7319eda4
AdityaGaur7 <adgaur027>
2023-03-09 11:43:50 +0530

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+function funct1() {
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("menu-icon").style.display = "none";
+  document.getElementById("down-arrow").style.display = "flex";
+}
+function funct2() {
+  document.getElementById("menu").style.display = "flex";
+  document.getElementById("menu-icon").style.display = "flex";
+  document.getElementById("down-arrow").style.display = "none";
+}
